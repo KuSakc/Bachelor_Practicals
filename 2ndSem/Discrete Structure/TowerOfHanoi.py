fdaeb60521fbfbaf0612ad795035a01d140314fc
SkeletonFramework <sakshamparajuli.official@gmail.com>
2026-08-22 09:38:32 +0545

    Update TowerOfHanoi.py

--- 2ndSem/Discrete Structure/TowerOfHanoi.py
+++ 2ndSem/Discrete Structure/TowerOfHanoi.py
@@ -1,38 +1,47 @@
 class Hanoi:
+    move_counter: int = 0
+    completed: bool = False
 
-    move_counter = 0
-    completed = False
 
-    def __init__(self, A, B, C):
-        self.Tower_A = A
+    def __init__(self, A: list[tuple[str,int]], B: list[tuple[str,int]], C: list[tuple[str,int]]):
+        self.Tower_A= A
         self.Tower_B = B
         self.Tower_C = C
 
-    getWeight = lambda Tower: Tower[0][1]
 
-    def judgeCompletion(self):
-        if self.Tower_A.count() == 3:
+    getWeight= lambda Tower: Tower[0][1]
+
+    def judgeCompletion(self) -> None:
+        if len(self.Tower_A) == 3:
             print("Hanoi Completed in Tower A")
-        elif self.Tower_B.count() == 3:
+            self.completed = True
+        elif len(self.Tower_B) == 3:
             print("Hanoi Completed in Tower B")
-        elif self.Tower_C.count() == 3:
+            self.completed = True
+        elif len(self.Tower_C) == 3:
             print("Hanoi Completed in Tower C")
+            self.completed = True
 
 
-    def judgeValidity(self, TowerPrev, TowerNew):
+    def judgeValidity(self, TowerPrev: list[tuple[str,int]], TowerNew: list[tuple[str,int]]) -> bool:
         if self.getWeight(TowerNew) > self.getWeight(TowerPrev):
              return True
         else:
             return False
 
 
-    def moveDisk(self, TowerPrev, TowerNew):
+    def moveDisk(self, TowerPrev: list[tuple[str,int]], TowerNew: list[tuple[str,int]]):
             if self.judgeValidity(TowerPrev, TowerNew):
-                 return True
+                 self.move_counter+=1
             else:
-                 return False
+                 pass
 
+def main():
+    hanoi = Hanoi([("Disk",1)],[("Disk",2)], [("Disk",3)])
+    print(hanoi.move_counter)
 
+if __name__ == '__main__':
+    main()
 
 
     # Tower_A = [("Disk",1)]
