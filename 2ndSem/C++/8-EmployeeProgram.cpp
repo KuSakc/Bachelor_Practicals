a03729865057ed0fc8d5fd8608e7c972992dc1f8
Saksham Parajuli <sakshamparajuli.official@gmail.com>
2026-08-24 14:43:36 +0545

    Update 8-EmployeeProgram.cpp

--- 2ndSem/C++/8-EmployeeProgram.cpp
+++ 2ndSem/C++/8-EmployeeProgram.cpp
@@ -1,29 +1,33 @@
 #include <string>
+#include <iostream>
+
 class Employee
 {
     private:
-        int id,salary;
-        string name;
+        int id, salary;
+        std::string name;
 
     public:
-        Employee(int id,int salary,string name)
-    {
-        this -> id = id;
-        this -> salary = salary;
-        this -> name = name;
-    }
-        void displayDetails(){
+        Employee(int id, int salary, std::string name)
+        {
+            this->id = id;
+            this->salary = salary;
+            this->name = name;
+        }
+
+        void displayDetails()
+        {
             std::cout << "Name: " << name << std::endl;
             std::cout << "ID: " << id << std::endl;
             std::cout << "Salary: " << salary << std::endl;
             std::cout << "---" << std::endl;
         }
-}
+};  
 
 int main()
 {
-    Employee Kusa(36, 50000,"Kusa KC");
-    Kusa.displayDetails()
+    Employee Kusa(36, 50000, "Kusa KC");
+    Kusa.displayDetails();   
 
     return 0;
 }
