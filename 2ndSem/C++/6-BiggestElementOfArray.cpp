9744bc7c1962427a07d318497cc254a0a6671fc3
Saksham Parajuli <sakshamparajuli.official@gmail.com>
2026-08-25 22:21:38 +0545

    Update 6-BiggestElementOfArray.cpp

--- 2ndSem/C++/6-BiggestElementOfArray.cpp
+++ 2ndSem/C++/6-BiggestElementOfArray.cpp
@@ -1,17 +1,24 @@
-#include<iostream>
-int main()
-{
-    int arr[] = {1, 2, 34, 5, 6};
-    int arr_length = sizeof(arr) / sizeof(arr[0]); 
+#include <iostream>
 
-    int i, greatest=arr[0];
-    for (i=1; i<arr_length; i++)
+int findGreatest(int arr[], int length)
+{
+    int greatest = arr[0];
+    for (int i = 1; i < length; i++)
     {
-        if (arr[i]>greatest)
+        if (arr[i] > greatest)
         {
             greatest = arr[i];
         }
     }
+    return greatest;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 34, 5, 6};
+    int arr_length = sizeof(arr) / sizeof(arr[0]);
+
+    int greatest = findGreatest(arr, arr_length);
 
     std::cout << "Greatest Element of array is " << greatest << std::endl;
 
