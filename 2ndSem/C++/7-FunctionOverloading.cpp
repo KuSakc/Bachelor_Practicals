fc1bf282efbcd712d3606b1a6a5aa33ed4a9e8e5
Saksham Parajuli <sakshamparajuli.official@gmail.com>
2026-08-25 22:27:21 +0545

    Update 7-FunctionOverloading.cpp

--- 2ndSem/C++/7-FunctionOverloading.cpp
+++ 2ndSem/C++/7-FunctionOverloading.cpp
@@ -11,14 +11,12 @@ int area(int length) {
 int main() {
     int area_rectangle, area_square, length, breadth;
 
-    std::cout << "Enter length and breadth of rectangle: "<<std::endl;
+    std::cout << "Enter length and breadth of rectangle: "<<;
     std::cin >> length >> breadth;
 
     area_rectangle = area(length, breadth);
 
     std::cout << "Area of rectangle: " << area_rectangle <<std::endl;
-
-    std::cout << "Enter length of the square: "<<std::endl;
 
     std::cin >> length;
 
